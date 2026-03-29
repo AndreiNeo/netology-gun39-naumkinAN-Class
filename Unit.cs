865ec629e45552cd402bbf82994f0e39dde3655a
KOT <andreioneplus@gmail.com>
2026-03-29 15:23:06 +0300

    add Task

---
+++ Unit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_Class
+{
+    public class Unit
+    {
+        private float _health;
+        private float _armor;
+        public string Name { get; }
+        public float Health => _health;
+        public int Damage { get; } = 5;
+        public float Armor => _armor = 0.6f;
+        public Unit() : this(name: "Unknown Unit")
+        {
+        }
+        public Unit(string name)
+        {
+            Name = name;
+        }
+        public float GetRealHealth()
+        {
+            return Health * (1f + Armor);
+        }
+        //public (int,bool) SetDamage()
+        //{
+        //    _armor = 10;
+        //    if (Health <= 0)
+        //    {
+        //        return true;
+        //    } 
+        //    return false;
+        //}
+    }
+}
--- obj/Debug/net10.0/Homework Class.AssemblyInfo.cs
+++ obj/Debug/net10.0/Homework Class.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Homework Class")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aae10a5ad82eb8dcf82978a670307cc74e4d0dd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Homework Class")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Homework Class")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
