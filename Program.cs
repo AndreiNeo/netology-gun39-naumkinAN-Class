aae10a5ad82eb8dcf82978a670307cc74e4d0dd6
KOT <andreioneplus@gmail.com>
2026-03-29 11:27:37 +0300

    add project

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
